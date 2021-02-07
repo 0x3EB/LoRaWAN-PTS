04c2e268f5294aa450288aea1502a27a2fd39ea5
SebastienML <sebastien.lejeune6@gmail.com>
2021-02-07 12:33:41 +0100

    update sending/receiving data

--- pycom/1_join_network.py
+++ pycom/1_join_network.py
@@ -1,6 +1,7 @@
 from network import LoRa
 import time
 import binascii
+import socket
 
 lora = LoRa(mode=LoRa.LORAWAN, region= LoRa.EU868)
 
@@ -15,3 +16,22 @@ while not lora.has_joined():
     print('Not joined yet...')
 
 print('Network joined!')
+
+#send message
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
+s.setblocking(False)
+s.send(bytes([1,2,3]))
+print('Message sent')
+
+#receive message
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
+s.setblocking(False)
+s.send(bytes([1,2,3]))
+s.settimeout(3.0)
+try:
+    rx_pkt = s.recv(64)
+    print(rx_pkt)
+except socket.error:
+    print("No packet receive")
